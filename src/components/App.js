c4e3b4094c05147d79feb58c2310b9e473498782
David Guenther <davidcarlguenther@gmail.com>
2019-12-10 16:14:58 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+
+        //bind methods to instance here
+    }
+
+    //event-handlers
+
+    render() {
+        return (
+            <h1>Todo App</h1>
+        )
+    }
+}
+
+export default App;
